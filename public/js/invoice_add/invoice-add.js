16a9f44c4ac7d36d9b23937c6e70335a901e0557
= <=>
2019-02-15 21:21:36 +0200

    forget to add those to my first commit

---
+++ public/js/invoice_add/invoice-add.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+
+    $repeater = $('.repeater').repeater({
+        // (Optional)
+        // start with an empty list of repeaters. Set your first (and only)
+        // "data-repeater-item" with style="display:none;" and pass the
+        // following configuration flag
+//                initEmpty: true,
+        // (Optional)
+        // "defaultValues" sets the values of added items.  The keys of
+        // defaultValues refer to the value of the input's name attribute.
+        // If a default value is not specified for an input, then it will
+        // have its value cleared.
+        defaultValues: {
+            'text-input': 'foo'
+        },
+        // (Optional)
+        // "show" is called just after an item is added.  The item is hidden
+        // at this point.  If a show callback is not given the item will
+        // have $(this).show() called on it.
+        show: function () {
+            index = $('.invoice-item').length;
+            $(this).find('input,select').each(function () {
+                newId = $(this).data('id') + '-' + index;
+                while ($('#'+newId).length > 0)
+                {
+                    newId = $(this).data('id') + '-' + index;
+                    index++;
+                }
+                $(this).attr('id', newId);
+            })
+            $(this).slideDown();
+        },
+        // (Optional)
+        // "hide" is called when a user clicks on a data-repeater-delete
+        // element.  The item is still visible.  "hide" is passed a function
+        // as its first argument which will properly remove the item.
+        // "hide" allows for a confirmation step, to send a delete request
+        // to the server, etc.  If a hide callback is not given the item
+        // will be deleted.
+        hide: function (deleteElement) {
+            if(confirm('Are you sure you want to delete this element?')) {
+                $(this).slideUp(deleteElement);
+            }
+        },
+        // (Optional)
+        // You can use this if you need to manually re-index the list
+        // for example if you are using a drag and drop library to reorder
+        // list items.
+        ready: function (setIndexes) {
+            console.log(setIndexes);
+        },
+        // (Optional)
+        // Removes the delete button from the first list item,
+        // defaults to false.
+        isFirstItemUndeletable: true
+    })
+
+    function calculateTotals()
+    {
+        total = 0;
+        console.log('123');
+        $('.invoice-item').each(function () {
+            itemPrice = $(this).find('.invoice-item__price').val();
+            itemQunatity = $(this).find('.invoice-item__qunatity').val();
+            if(itemPrice && itemQunatity)
+            {
+                itemSubtotal = itemPrice * itemQunatity;
+                total += itemSubtotal;
+                $(this).find('.invoice-item__subtotal').val(itemSubtotal);
+            }
+        })
+        $('#total').val(total);
+    }
+
+    $('body').on('change', '.invoice-item__price,.invoice-item__qunatity', calculateTotals);
+    $(function () {
+        $('#date').datepicker();
+        $('form').parsley();
+    })
+//
+//            $repeater.setList(
+//                [
+//                    {
+//                        product_id: 1,
+//                        quantity: 2,
+//                        subtotal: 15,
+//                        price: 10,
+//                    }
+//                ]
+//            )
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
+    </head>
+    <body>
+    <div class="container">
+        <h1 class="text-center">Invoice</h1>
+        <form class="repeater">
+            <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="date">Date</label>
+                    <input  data-parsley-required="true"	 id="date" type="text" name="date" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="client_id">Client</label>
+                    <select
+                                    data-parsley-required="true"	 class="form-control" id="client_id" name="client_id">
+                        <option value="1">belal</option>
+                        <option value="2">ahmed</option>
+                    </select>
+                </div>
+            </div>
+                <div class="my-2 col-12 text-center"><h2>Invoice Items</h2></div>
+            <div class="col-12">
+                <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>SubTotal</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody data-repeater-list="InvoiceItem">
+                      <tr data-repeater-item class="invoice-item">
+                        <td>1</td>
+                        <td>
+                            <select  data-parsley-required="true" data-id="product_id" class="form-control" id="product_id-1" name="InvoiceItem[0][product_id]">
+                                <option value="1">belal</option>
+                                <option value="2">ahmed</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input  data-parsley-required="true" data-id="quantity" id="quantity-1" type="text" name="InvoiceItem[0][quantity]" class="invoice-item__qunatity form-control"/>
+                        </td>
+                        <td>
+                            <input  data-parsley-required="true" data-id="price" id="price-1" type="text" name="InvoiceItem[0][price]" class="invoice-item__price form-control"/>
+                        </td>
+                        <td>
+                            <input  value="0" data-id="subtotal" readonly id="subtotal-1" name="InvoiceItem[0][subtotal]" type="text"  class=" invoice-item__subtotal form-control"/>
+                        </td>
+                        <td>
+                            <input class="btn btn-danger" data-repeater-delete type="button" value="X"/>
+                        </td>
+                      </tr>
+
+
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="4">
+                            Total
+                        </td>
+                        <td >
+                            <input type="number" id="total" readonly name="Invoice[total]" class="form-control">
+                        </td>
+                        <td></td>
+                    </tr>
+                    </tfoot>
+                  </table>
+                <div class="row">
+                    <div class="col-12 text-right">
+                        <button type="button" data-repeater-create class="text-center btn btn-success">+</button>
+                    </div>
+                </div>
+            </div>
+                <div class="mt-5 col-12 text-center">
+                    <button name="submit" class="btn btn-lg btn-success" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script type="text/javascript" src="{{URL::asset('js/app.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('js/invoice_add/invoice-add.js')}}"></script>
+
+    </body>
+</html>
