d0ad01a9d3d46f2b0e9338c1ccc5898a4750b3ec
= <=>
2019-02-15 22:05:45 +0200

    added models and migrations

---
+++ database/migrations/2019_02_15_193419_create_invoices_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->float('total');
+            $table->float('paid');
+            $table->float('unpaid');
+            $table->tinyInteger('status');
+            $table->tinyInteger('type');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
---
+++ database/migrations/2019_02_15_194343_create_invoice_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoiceItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+            $table->float('quantity');
+            $table->float('item_price');
+            $table->float('item_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+}
